ed9f052094089b44f995a79f3cd926c24b2b8e91
Cody Goss <c.ward.goss@gmail.com>
2019-04-15 20:46:03 -0400

    inti commit

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Test />
+      </div>
+    );
+  }
+}
+
+module.exports = App;
---
+++ app/index.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const App = require('./components/App');
+
+ReactDOM.render(<App />, document.getElementById('app'));
