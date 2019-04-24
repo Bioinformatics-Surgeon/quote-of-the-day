07e5f5520a1cfdf41583b2e6f4621df1da85fdaa
Cody Goss <c.ward.goss@gmail.com>
2019-04-24 13:48:40 -0400

    adds Contanct component

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -3,8 +3,46 @@ const React = require('react');
 function Contact() {
   return (
     <div className='container'>
+      <img
+        src='https://res.cloudinary.com/moodyappcloudname/image/upload/v1556126996/2400x2400Headshot.jpg'
+        alt='Picture of Cody Goss'
+        className='avatar'
+      />
       <h1 className='header'>Cody Goss</h1>
-      <h2 className='sub-header'>Email</h2>
+      <ul>
+        <li className='list-item'>
+          LinkedIn
+          <a
+            href='https://www.linkedin.com/in/mudanic/'
+            class='btn btn-primary'
+            target='_blank'
+          >
+            <i class='fab fa-linkedin' />
+          </a>
+        </li>
+        <li className='list-item'>
+          Twitter
+          <a
+            href='https://twitter.com/TeaChaboshi?lang=en'
+            class='btn btn-primary'
+            target='_blank'
+          >
+            <i class='fab fa-twitter-square' />
+          </a>
+        </li>
+        <li className='list-item'>
+          GitHub
+          <a
+            href='https://github.com/Bioinformatics-Surgeon'
+            class='btn btn-primary'
+            target='_blank'
+          >
+            <i class='fab fa-github-square' />
+          </a>
+        </li>
+        <li className='list-item'>Website</li>
+        <li className='list-item'>Email</li>
+      </ul>
     </div>
   );
 }
