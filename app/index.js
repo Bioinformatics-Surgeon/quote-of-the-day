1d63ce6e0fad00ea6f6f3cbc143febb0a32ae16c
Cody Goss <c.ward.goss@gmail.com>
2019-04-30 11:01:55 -0400

    adds a 404 page to handle refresh and potential 404s

--- app/index.js
+++ app/index.js
@@ -3,4 +3,5 @@ const ReactDOM = require('react-dom');
 const App = require('./components/App');
 require('./index.css');
 
+// This is how we render to the dom
 ReactDOM.render(<App />, document.getElementById('app'));
